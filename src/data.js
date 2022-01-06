dcf4deca11a9642ef0dc52385f0795ffab0f57fe
fabohlus <45484002+fabohlus@users.noreply.github.com>
2022-01-06 13:22:42 +0100

    share code on github

---
+++ src/data.js
@@ -0,0 +1,65 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name: 'Air Jordan 1 A Ma Maniere',
+            image:'/images/p1.jpg',
+            price: 420.99,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews: 10,
+            description: 'Collab of Nike and A Ma Maniere'
+        },
+        {
+            _id:'2',
+            name: 'Nike Kobe 6 Protro Grinch',
+            image:'/images/p2.jpg',
+            price: 900.00,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews: 7,
+            description: 'Last Kobe Shoe'
+        },
+        {
+            _id:'3',
+            name: 'Nike Blazer 77 High',
+            image:'/images/p3.jpg',
+            price: 259.99,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews: 22,
+            description: 'Just a normal 77 High'
+        },
+        {
+            _id:'4',
+            name: 'Nike Yeezy Red October',
+            image:'/images/p4.jpg',
+            price: 5999.99,
+            brand:'Nike',
+            rating:'4.5',
+            numReviews: 56,
+            description: 'First Yeezy Ever'
+        },
+        {
+            _id:'5',
+            name: 'Nike Air Force 1 Chicago',
+            image:'/images/p5.jpg',
+            price: 600.00,
+            brand:'Nike',
+            rating:'3.5',
+            numReviews: 14,
+            description: 'Coolest Jordan 1'
+        },
+        {
+            _id:'6',
+            name: 'Nike Protro Off White',
+            image:'/images/p6.jpg',
+            price: 945.99,
+            brand:'Nike',
+            rating:'5.0',
+            numReviews: 44,
+            description: 'Way too expensive'
+        },
+    ],
+};
+export default data;
